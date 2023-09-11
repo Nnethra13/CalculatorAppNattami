4ea88daf0b6c20ed31026cdb15aa847face45742
Nnethra13 <113718001+Nnethra13@users.noreply.github.com>
2023-09-11 06:54:35 -0500

    added a number buttons

--- app/src/main/java/com/example/calculatorappnattami/MainActivity.java
+++ app/src/main/java/com/example/calculatorappnattami/MainActivity.java
@@ -15,26 +15,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void getTextValues(View v) {
+
+    @Override
+    public void onClick(View v){
+
+    }
+    public void getInputs(View v) {
         // reference to EditText's by locating its elements in the xml file
         // then convert to string then integer
 
         Button buttonAdd = findViewById(R.id.addButton);
         Button buttonSubtract = findViewById(R.id.subtractButton);
+        Button buttonDivide = findViewById(R.id.divideButton);
+        Button buttonMultiply = findViewById(R.id.multiplyButton);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+
+
+
+
 
 
 
         // input 1
         EditText userInputEt1 = findViewById(R.id.inputEditNumberDecimal1);
-        int input1 = userInputEt1.getInputType();
+        double input1 = userInputEt1.getInputType();
 
         // input 2
         EditText userInputEt2 = findViewById(R.id.inputEditNumberDecimal2);
-        int input2 = userInputEt2.getInputType();
+        double input2 = userInputEt2.getInputType();
+
+
 
 
         // don't forget to CALL this method with the onClick
     }
+    public void solve(){
+
+    }
+
 
     public void buttonSelect(View v){
         int numInput;
@@ -70,8 +96,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    @Override
-    public void onClick(View view) {
 
-    }
+
 }
